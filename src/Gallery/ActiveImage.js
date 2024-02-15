a0f17faf809c70159426d2d00206379755890ccc
Chris Hammock <chris@crshmk.com>
2024-02-15 19:15:48 +0700

    add image loading state

--- src/Gallery/ActiveImage.js
+++ src/Gallery/ActiveImage.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import useGallery from './useGallery'
 
+import imagePlaceholder from '../assets/img/image-placeholder.png'
+
 const ActiveImage = () => {
-  const { activeImage } = useGallery()
+  const { activeImage, activeImageIndex } = useGallery()
   const [hasLoaded, setHasLoaded] = useState(false)
+
+  useEffect(() => {
+    setHasLoaded(false)
+  }, [activeImageIndex])
 
   const imgClassName = !hasLoaded ? 'loading' : ''
 
   return (
     <div className="active-img">
-      <img className={imgClassName} src={activeImage} onLoad={() => setHasLoaded(true)}/>
+      {!hasLoaded && <img src={imagePlaceholder} />}
+      <img 
+        className={imgClassName} 
+        src={activeImage} 
+        onLoad={() => setHasLoaded(true)}
+      />
     </div>
   )
 }
