46946c18e7b8cacc41bd68ede5406ea330d5a3f6
Chris Hammock <chris@crshmk.com>
2024-02-15 15:18:12 +0700

    basic gallery layout

--- src/Page.js
+++ src/Page.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
-import sunbeam from './sunbeam.jpeg'
+import temple from './assets/img/laos-temple.jpg'
 
-const Page = () => 
-  <img src={sunbeam} />
+const Bullet = () => (
+  <div className="bullet" style={{backgroundImage: `url(${temple})`}}>
+    
+  </div>
+)
+
+const Label = () => (
+  <div className="label">
+      <span>01 / 04</span>
+      <span>Section</span>
+  </div>
+)
+
+const Page = () => (
+  <div className="gallery">
+    <div className="active-img">
+    <img src={temple} />
+    </div>
+    <div className="bullets">
+      <Bullet />
+      <Bullet />
+      <Bullet />
+      <Bullet />
+    </div>
+    <Label />
+  </div>
+)
 
 export default Page
