6f296e6d9403d5ce07763f273c255278dc1e42c1
Chris Hammock <chris@crshmk.com>
2024-02-15 17:25:16 +0700

    basic image loading state

--- src/Gallery/ActiveImage.js
+++ src/Gallery/ActiveImage.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import useGallery from './useGallery'
 
 const ActiveImage = () => {
   const { activeImage } = useGallery()
+  const [hasLoaded, setHasLoaded] = useState(false)
+
+  const imgClassName = !hasLoaded ? 'loading' : ''
 
   return (
     <div className="active-img">
-      <img src={activeImage} />
+      <img className={imgClassName} src={activeImage} onLoad={() => setHasLoaded(true)}/>
     </div>
   )
 }
